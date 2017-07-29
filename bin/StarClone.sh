fe3672755f32046c3fe40e5b9e467060349cd469
banzay <infbonzay@gmail.com>
2017-07-29 15:30:40 +0300

    smkview tune

--- bin/StarClone.sh
+++ bin/StarClone.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 PREVDIR=`pwd`
 MYPATH=`dirname $0`
+
 cd "$MYPATH"
 MYPATH=`pwd`
-
 env "LD_LIBRARY_PATH=$MYPATH/libs:$LD_LIBRARY_PATH" "$MYPATH/StarClone" "$@"
-#env "LD_LIBRARY_PATH=$MYPATH/libs:$LD_LIBRARY_PATH" ldd "$MYPATH/StarClone"
 cd "$PREVDIR"
