5e4219df4c02e027186f4d4003843c4b0c83dbcc
vbotnari <vbotnari@starnet.md>
2018-10-19 17:33:32 +0300

    implement cmake checking library version, remove unused scrips code

--- cmake/Modules/Findgrp.cmake
+++ cmake/Modules/Findgrp.cmake
@@ -5,26 +5,7 @@
 #  grp_INCLUDE_DIRS - the grplib include directories
 #  grp_LIBRARIES - link these to use grplib
 
-include(LibFindMacros)
-
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(grp_PKGCONF grp)
+find_package(PkgConfig)
 
-# Main include dir
-find_path(grp_INCLUDE_DIR
-  NAMES grp.h
-  PATHS ${grp_PKGCONF_INCLUDE_DIRS}
-  PATH_SUFFIXES grp
-)
-
-# Finally the library itself
-find_library(grp_LIBRARY
-  NAMES grp
-  PATHS ${grp_PKGCONF_LIBRARY_DIRS}
-)
-
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(grp_PROCESS_INCLUDES grp_INCLUDE_DIR grpConfig_INCLUDE_DIR)
-set(grp_PROCESS_LIBS grp_LIBRARY)
-libfind_process(grp)
+pkg_check_modules(grp grp>=0.4.20)
--- cmake/Modules/Findmpq.cmake
+++ cmake/Modules/Findmpq.cmake
@@ -5,26 +5,7 @@
 #  mpq_INCLUDE_DIRS - the mpqlib include directories
 #  mpq_LIBRARIES - link these to use mpqlib
 
-include(LibFindMacros)
-
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(mpq_PKGCONF mpq)
+find_package(PkgConfig)
 
-# Main include dir
-find_path(mpq_INCLUDE_DIR
-  NAMES StormLib.h
-  PATHS ${mpq_PKGCONF_INCLUDE_DIRS}
-  PATH_SUFFIXES mpq
-)
-
-# Finally the library itself
-find_library(mpq_LIBRARY
-  NAMES mpq
-  PATHS ${mpq_PKGCONF_LIBRARY_DIRS}
-)
-
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(mpq_PROCESS_INCLUDES mpq_INCLUDE_DIR mpqConfig_INCLUDE_DIR)
-set(mpq_PROCESS_LIBS mpq_LIBRARY)
-libfind_process(mpq)
+pkg_check_modules(mpq mpq>=0.5.3)
